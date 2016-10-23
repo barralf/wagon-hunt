f6baa53d5e51b3ed87b9859a4cc43695c696c403
Florian Barral <florianbarral@hotmail.com>
2016-10-23 13:24:47 +0200

    lets encrypt collective idea :new id

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def letsencrypt
-    render text: "pDaUwzUsv2z_F5AZlRcmaE-k0Bj6VQGHEhnpeeavbbE.DZBSrD2G3yc7tsRmS6QSNOD_xqCYlw_UhoxkYYrUv3k"
+    render text: "5zDDF5tHgRYnImxjJDOuP6WnbZpwdXfKBmtSnHTi-40.DZBSrD2G3yc7tsRmS6QSNOD_xqCYlw_UhoxkYYrUv3k"
   end
 
   def home
